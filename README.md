0f05ce60aeafee782c3778fc2cd7dac376720692
Lorenzo Bernini <templarec@gmail.com>
2021-04-14 23:36:55 +0200

    Esercizio completato - comments e Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Tempo impiegato progetto: 14 ore
+Tempo impiegato progetto: 15 ore <br>
+
+Esercizio base (4 milestone)<br>
+Esercizio bonus (2 milestone)<br>
+<br>
+api custom per adattare le sigle del sito delle bandiere <br>
+countryflags.io con le sigle standard ISO-639-1 di moviedb <br>
+<br>
+pagination 
--- main.js
+++ main.js
@@ -1,46 +1,49 @@
 //vue-boolflix - 12/04/2021
 // Author: Lore
-/*$(document).ready(function () {
-
-});*/
 var app = new Vue({
 	el: '#root',
 	data: {
-		searchTxt: '',
-		searchResult: [],
-		searchResultTv: [],
-		currentPagina: 1,
-		totPagine: 0,
-		baseImgPath: '',
-		currentMovieCast: [],
-		tipo: 'film',
-		currentGeneri: [],
-		currentGeneriTv: [],
-		generiSearch: [],
-		generiSearchTv: [],
-		breadCrumb: [],
-		selectedGenere: '',
-		selectedGenereTv: '',
-		breadCrumbTv: [],
-		totPagineTv: 0,
-		currentPaginaTv: 1,
-		totalResults: 0,
-		totalResultsTv: 0,
-		currentFlag: ''
+		searchTxt: '', //bind input txt ricerca
+		searchResult: [], //array risultati film
+		searchResultTv: [], //array risultati serietv
+		currentPagina: 1, //pagina corrente film
+		totPagine: 0,	//numero di pagine totali risultati film
+		baseImgPath: '',	//path delle immagini
+		currentMovieCast: [], //array cast
+		tipo: 'film',	//per le schede film/serie tv
+		currentGeneri: [],	// tutti i generi tv
+		currentGeneriTv: [], //tutti i generi serie tv
+		generiSearch: [], //generi del risultato di ricerca film
+		generiSearchTv: [], //generi del risultato di ricerca serie tv
+		breadCrumb: [], //array generi linkati tra id e nomi per visualizzare in pagina da selezionare film
+		selectedGenere: '', //genere selezionato in breadcrumb film
+		selectedGenereTv: '', //genere selezionato in breadcrumb serie tv
+		breadCrumbTv: [], //array generi linkati tra id e nomi per visualizzare in pagina da selezionare serie tv
+		totPagineTv: 0, //totale pagine dei risultati serie tv
+		currentPaginaTv: 1, //pagina corrente serie tv
+		totalResults: 0, //risultati totali film
+		totalResultsTv: 0, //risultati totali serie tv
+		currentFlag: '' // bandiera corrente
 	},
 	mounted () {
+		//prendo i path delle immagini dal api config
 		axios.get('https://api.themoviedb.org/3/configuration?api_key=db6e548b4cda3f3d5550a22268a7e90c')
 			.then((configs) => {
+				//seleziono la larghezza delle immagini
 				let size = configs.data.images.poster_sizes[3];
+				// prendo uri di base
 				let base = configs.data.images.base_url;
+				//assemblo per ottenere il path completo
 				this.baseImgPath = base+size;
 
 			})
+		//mi salvo tutti i generi dei film
 		axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=db6e548b4cda3f3d5550a22268a7e90c')
 			.then((risposta) => {
 				this.currentGeneri = risposta.data.genres
 
 			})
+		//mi salvo tutti i generi serie tv
 		axios.get('https://api.themoviedb.org/3/genre/tv/list?api_key=db6e548b4cda3f3d5550a22268a7e90c')
 			.then((risposta) => {
 				this.currentGeneriTv = risposta.data.genres
@@ -49,13 +52,20 @@ var app = new Vue({
 	},
 	computed: {},
 	methods: {
+		//ricerca per campo input
 		searchMovies: function (searchTxt) {
+			//in base al testo inserito e alla pagina corrente (parte da 1 come da data:)
 			axios.get('https://api.themoviedb.org/3/search/movie?api_key=db6e548b4cda3f3d5550a22268a7e90c&query='+ searchTxt + '&page=' + this.currentPagina)
 				.then((risposta) => {
+					//salvo in array i risultati
 					this.searchResult = risposta.data.results;
+					// salvo il numero di pagine totali
 					this.totPagine = risposta.data.total_pages;
+					// salvo il numero di risultati totali
 					this.totalResults = risposta.data.total_results;
+					//azzero array generi della ricerca corrente prima di ogni nuova query
 					this.generiSearch = [];
+					//stessa cosa per i link dei generi del breadcrumb
 					this.breadCrumb = [];
 					//mi salvo in array tutti i generi della ricerca
 					risposta.data.results.forEach((item)=>{
@@ -68,7 +78,7 @@ var app = new Vue({
 						})
 
 					})
-					//associo id e name e li pusho in nuovo array
+					//associo id e name e li pusho in nuovo array (perchè sono in due array diversi)
 					this.currentGeneri.forEach((item)=>{
 						this.generiSearch.forEach((elemento)=>{
 							if (item.id === elemento) {
@@ -83,10 +93,8 @@ var app = new Vue({
 						})
 
 					})
-
-					// this.pagina = risposta.data.page;
-					// this.totPagine = risposta.data.total_pages;
 				})
+			// ricerca serie tv (stessa cosa delle righe sopra 61-99
 			axios.get('https://api.themoviedb.org/3/search/tv?api_key=db6e548b4cda3f3d5550a22268a7e90c&query='+ searchTxt+ '&page=' + this.currentPaginaTv)
 				.then((risposta) => {
 					this.searchResultTv = risposta.data.results;
@@ -118,346 +126,47 @@ var app = new Vue({
 					})
 				})
 		},
+		//conversione bandiere per adattarlo alla sorgente delle immagini delle bandiere
 		apiFlags: function (sigla_in)  {
+			//prendo i dati dalla mia api dove sono associate sigla lingua con bandiera giusta
+			//riceve in ingresso la lingua da movie db e restituisce la sigla della bandiera da usare
+			//nel sito countryflags.io
 			axios.get(`https://www.bearwebdesign.it/api/flag/index.php?query=${sigla_in}`)
 				.then((risposta)=>{
 					this.currentFlag = risposta.data.flag[0].sigla_out
-					/*let bandiere = risposta.data.flag
-
-					bandiere.forEach((item)=>{
-						if (item.sigla_in === sigla_in){
-							 this.currentFlag = item.sigla_out
-
-						}
-
-					})*/
-
 				})
 
 		},
-
-		/*getFlag: function (lingua) {
-
-			switch (lingua) {
-				case "en":
-					return 'gb';
-				case "ja":
-					return 'jp';
-				case "af":
-					return 'za';
-				case "ak":
-					return 'gh';
-				case "am":
-					return 'et';
-				case "ar":
-					return 'ae';
-				case "as":
-					return 'in';
-				case "ba":
-					return 'ru';
-				case "be":
-					return 'by';
-				case "bi":
-					return 'vu';
-				case "bm":
-					return 'ml';
-				case "bn":
-					return 'bd';
-				case "bo":
-					return 'cn';
-				case "br":
-					return 'fr';
-				case "bs":
-					return 'ba';
-				case "ca":
-					return 'es';
-				case "ch":
-					return 'gu';
-				case "co":
-					return 'fr';
-				case "cr":
-					return 'ca';
-				case "cu":
-					return 'rs';
-				case "cv":
-					return 'ru';
-				case "cy":
-					return 'gb';
-				case "dz":
-					return 'bt';
-				case "ee":
-					return 'tg';
-				case "et":
-					return 'ee';
-				case "eu":
-					return 'fr';
-				case "ga":
-					return 'ie';
-				case "gd":
-					return 'ie';
-				case "gl":
-					return 'es';
-				case "gn":
-					return 'bo';
-				case "gu":
-					return 'in';
-				case "kg":
-					return 'cd';
-				case "ki":
-					return 'ke';
-				case "km":
-					return 'kh';
-				case "kn":
-					return 'in';
-				case "kr":
-					return 'ng';
-				case "kw":
-					return 'gb';
-				case "ky":
-					return 'cn';
-				case "la":
-					return 'it';
-				case "lb":
-					return 'lu';
-				case "li":
-					return 'nl';
-				case "lu":
-					return 'cd';
-				case "ml":
-					return 'in';
-				case "mn":
-					return 'mn';
-				case "mo":
-					return 'md';
-				case "mr":
-					return 'in';
-				case "ms":
-					return 'my';
-				case "my":
-					return 'mm';
-				case "na":
-					return 'nr';
-				case "ne":
-					return 'np';
-				case "ng":
-					return 'ne';
-				case "nr":
-					return 'za';
-				case "ny":
-					return 'zm';
-				case "om":
-					return 'et';
-				case "pa":
-					return 'pk';
-				case "ps":
-					return 'af';
-				case "rw":
-					return 'ug';
-				case "sa":
-					return 'in';
-				case "sc":
-					return 'it';
-				case "sd":
-					return 'pk';
-				case "se":
-					return 'no';
-				case "sg":
-					return 'cf';
-				case "sh":
-					return 'rs';
-				case "si":
-					return 'lk';
-				case "sl":
-					return 'si';
-				case "sm":
-					return 'ws';
-				case "sn":
-					return 'zw';
-				case "sr":
-					return 'rs';
-				case "ss":
-					return 'za';
-				case "st":
-					return 'ls';
-				case "sv":
-					return 'se';
-				case "ta":
-					return 'in';
-				case "tg":
-					return 'ir';
-				case "tk":
-					return 'tm';
-				case "tl":
-					return 'ph';
-				case "tn":
-					return 'bw';
-				case "tt":
-					return 'ru';
-				case "tw":
-					return 'gh';
-				case "ug":
-					return 'kz';
-				case "ve":
-					return 'za';
-				case "vi":
-					return 'vn';
-				case "za":
-					return 'cn';
-				case "aa":
-					return 'et';
-				case "ab":
-					return 'ge';
-				case "av":
-					return 'ru';
-				case "ay":
-					return 'ar';
-				case "ce":
-					return 'ru';
-				case "da":
-					return 'dk';
-				case "dv":
-					return 'mv';
-				case "el":
-					return 'gr';
-				case "fa":
-					return 'ir';
-				case "ff":
-					return 'bj';
-				case "fy":
-					return 'nl';
-				case "gv":
-					return 'im';
-				case "ha":
-					return 'ng';
-				case "he":
-					return 'il';
-				case "hi":
-					return 'in';
-				case "ho":
-					return 'pg';
-				case "hy":
-					return 'am';
-				case "hz":
-					return 'na';
-				case "ig":
-					return 'ng';
-				case "ii":
-					return 'cn';
-				case "ik":
-					return 'gl';
-				case "iu":
-					return 'ca';
-				case "jv":
-					return 'id';
-				case "ka":
-					return 'ge';
-				case "kj":
-					return 'ao';
-				case "kk":
-					return 'kz';
-				case "kl":
-					return 'gl';
-				case "ko":
-					return 'kp';
-				case "ks":
-					return 'in';
-				case "ku":
-					return 'tr';
-				case "kv":
-					return 'ru';
-				case "lg":
-					return 'ug';
-				case "ln":
-					return 'cd';
-				case "lo":
-					return 'la';
-				case "mi":
-					return 'nz';
-				case "nb":
-					return 'no';
-				case "nd":
-					return 'zw';
-				case "nn":
-					return 'no';
-				case "nv":
-					return 'us';
-				case "oc":
-					return 'fr';
-				case "oj":
-					return 'ca';
-				case "or":
-					return 'in';
-				case "os":
-					return 'ru';
-				case "pi":
-					return 'in';
-				case "qu":
-					return 'ar';
-				case "rm":
-					return 'it';
-				case "rn":
-					return 'bi';
-				case "sq":
-					return 'al';
-				case "su":
-					return 'sd';
-				case "sw":
-					return 'tz';
-				case "te":
-					return 'in';
-				case "ti":
-					return 'et';
-				case "ts":
-					return 'mz';
-				case "ty":
-					return 'pf';
-				case "uk":
-					return 'ua';
-				case "ur":
-					return 'in';
-				case "wa":
-					return 'be';
-				case "wo":
-					return 'sn';
-				case "xh":
-					return 'za';
-				case "yi":
-					return 'il';
-				case "yo":
-					return 'ng';
-				case "zh":
-					return 'cn';
-				case "zu":
-					return 'za';
-				case "cs":
-					return 'cz';
-				default:
-					return lingua;
-			}
-
-		},*/
+		//calcolo delle stelle in base al rating
 		calcStar: function (rating) {
+			//mi assicuro che il rating venga considerato come un float
 			let base10 = parseFloat(rating);
+			// lo divido per due e lo arrotondo
 			let base5 = Math.round(base10 / 2);
 			return base5
 		},
+		//richiesta api del cast in base all'id del film che gli passo
+		//e salvo in array
 		getCast: function (movieId) {
 			axios.get('https://api.themoviedb.org/3/movie/'+ movieId + '/credits?api_key=db6e548b4cda3f3d5550a22268a7e90c')
 				.then((risposta) => {
 					this.currentMovieCast = risposta.data.cast;
-
-				})
+			})
 		},
+		//richiesta api del cast in base all'id della serie tv che gli passo
+		//e salvo in array
 		getCastTv: function (movieId) {
 			axios.get('https://api.themoviedb.org/3/tv/'+ movieId + '/credits?api_key=db6e548b4cda3f3d5550a22268a7e90c')
 				.then((risposta) => {
 					this.currentMovieCast = risposta.data.cast;
-
 				})
 		},
+		//svuotamento array cast
 		purgeCast: function () {
 			this.currentMovieCast = []
 		},
+		//salvataggio id del genere che l'utente clicca nel breadcrumb serve per l'incrocio
+		//vedi metodo searchMovies alla fine
 		getSelGenere: function (genid) {
 			this.selectedGenere = genid
 			this.selectedGenereTv = genid
@@ -466,7 +175,6 @@ var app = new Vue({
 
 
 		}
-
 	}
 });
 
